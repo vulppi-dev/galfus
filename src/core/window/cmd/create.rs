b7301263dcb97f154d3bb544cdf5a59e48517f38
Renato Rodrigues <renato@vulppi.dev>
2026-02-19 17:10:29 -0300

    Refactor build bindings workflow to use branches instead of paths and update function signatures for clarity in WASM context

--- src/core/window/cmd/create.rs
+++ src/core/window/cmd/create.rs
@@ -49,8 +49,6 @@ pub struct CmdResultWindowCreate {
 
 #[cfg(not(feature = "wasm"))]
 pub use create_native::engine_cmd_window_create;
-#[cfg(all(feature = "wasm", target_arch = "wasm32"))]
-pub use create_wasm::engine_cmd_window_create;
 #[cfg(all(feature = "wasm", not(target_arch = "wasm32")))]
 pub use create_wasm::engine_cmd_window_create;
 #[cfg(all(feature = "wasm", target_arch = "wasm32"))]
